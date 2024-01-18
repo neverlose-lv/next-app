f5e87543cb6596c927d23581fe94389f43488240
neverlose-lv <volkk@inbox.lv>
2024-01-18 15:54:55 +0200

    fix build

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -1,16 +1,16 @@
 // import WelcomeTemplate from '@/app/emails/WelcomeTemplate';
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 // import { Resend } from 'resend';
 
 // const resend = new Resend(process.env.RESEND_API_KEY);
 
-// export async function POST() {
-//   await resend.emails.send({
-//     from: '...', // domain is required
-//     to: 'programmingwithmosh@gmail.com',
-//     subject: '...',
-//     react: <WelcomeTemplate name="Mosh" />,
-//   });
+export async function POST() {
+  // await resend.emails.send({
+  //   from: '...', // domain is required
+  //   to: 'programmingwithmosh@gmail.com',
+  //   subject: '...',
+  //   react: <WelcomeTemplate name="Mosh" />,
+  // });
 
-//   return NextResponse.json({});
-// }
+  return NextResponse.json({});
+}
